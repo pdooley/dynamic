df29cba43f337e439bda8143071adbc2ebca0a0b
Patrick Dooley <pdooley@vectorsolv.com>
2022-06-29 11:31:46 -0400

    ss

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { TableDataSource } from '@vsolv/ui-kit/table';
 
 @Component({
   selector: 'dynamic2-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+
+  constructor(source: TableDataSource){
+    console.log("constructor() called");
+  }
+
+  ngOnInit(){
+    console.log("ngOnInit() called.")
+
+ 
+
+  }
 }
--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,13 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { TableModule } from '@vsolv/ui-kit/table'
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { TablePageComponent } from './table-page/table-page.component';
 
+const config = {
+  apiKey: "AIzaSyBDki01XPbo8OvLGzYqm9C4iJ1dRBplq3g",
+  authDomain: "kwdemo-8f69c.firebaseapp.com",
+  projectId: "kwdemo-8f69c",
+  storageBucket: "kwdemo-8f69c.appspot.com",
+  messagingSenderId: "870054033678",
+  appId: "1:870054033678:web:55e09549bcb78a5e33bf5f"
+};
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, TablePageComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    TablePageComponent,
+    ],
+  imports: [
+    BrowserModule,
+    TableModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
